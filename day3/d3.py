f23b1ddad5e82b4fc25e47670bbcc42f559d0638
Andy Bell <bell.andy.m@gmail.com>
2022-12-03 09:47:20 -0500

    day3 part 2

--- day3/d3.py
+++ day3/d3.py
@@ -26,3 +26,22 @@ for i, let in enumerate(letters):
 # find scores
 total = sum([scores[c] for c in repeats])
 print(f"Answer for part 1: {total}")
+
+
+########################################
+# part 2
+start = 0
+end = len(lines)
+step = 3
+groups = []
+for i in range(start, end, step):
+    x = i
+    groups.append(lines[x : x + step])
+print(groups)
+
+part2_score = 0
+for s in groups:
+    a = list(set.intersection(*[set(e) for e in s]))[0]
+    # print(a)
+    part2_score += scores[a]
+print(f"Part 2 Answer: {part2_score}")
