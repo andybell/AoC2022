cd143fb7067417f0fb2eb530beb875c81eafd3eb
Andy Bell <bell.andy.m@gmail.com>
2022-12-04 20:54:02 -0500

    d4 part 2

--- day4/d4.py
+++ day4/d4.py
@@ -1,7 +1,5 @@
 # AoC 2022 day 4
-import string
-
-file = "example.txt"
+# file = "example.txt"
 file = "input.txt"
 with open(file) as f:
     lines = f.read().splitlines()
@@ -25,3 +23,22 @@ def findOverlap(str_pair):
 
 
 print(f"part 1 answer: {sum([findOverlap(s) for s in lines])}")
+
+#################################################################################
+
+# part 2
+def findIfAnyOverlap(str_pair):
+    f, s = str_pair.split(",")
+    f0, f1 = f.split("-")
+    s0, s1 = s.split("-")
+
+    r_f = set(range(int(f0), int(f1) + 1))
+    r_s = set(range(int(s0), int(s1) + 1))
+
+    if r_f & r_s:
+        return True
+    else:
+        return False
+
+
+print(f"part 2 answer: {sum([findIfAnyOverlap(s) for s in lines])}")
